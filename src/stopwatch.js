7daffcee44ee6ffa6ffce666d7b8b06c202e2ad2
umang Malviya <umangmalviya51@gmail.com>
2024-07-29 18:43:56 +0530

    my second commit

--- src/stopwatch.js
+++ src/stopwatch.js
@@ -1,30 +1,45 @@
-import React, { useEffect,useState } from 'react'
+import React, { useState } from "react";
 
-const Stopwatch=()=>{
-const [time,setTime]=useState(0);
-const [isRunning,setRunning]=useState(false);
-const  startTime=()=>{
-    setInterval(()=>{
-    setTime(prev =>prev+1)
-    },1000)
-    setRunning(true)
-   
-}
+const Stopwatch = () => {
+  const [time, setTime] = useState({ sec: 0, milli: 0 });
+  const [status, setStatus] = useState();
+  let timesecond = time.sec;
+  let timemillisec = time.milli;
+  const [isRunning, setRunning] = useState(false);
+  const startTime = (timesecond, timemillisec) => {
+    myFun();
+    setStatus(setInterval(myFun, 10));
+    setRunning(true);
+  };
+  const stopTime = () => {
+    if (isRunning) {
+      clearInterval(status);
+      setRunning(false);
+    }
+  };
+  const endTime = () => {
+    clearInterval(status);
+    setTime({ sec: 0, milli: 0 });
+  };
 
-    return (
-     <div>
-    Stopwatch
-    <div >
-        Time:{time}
+  const myFun = () => {
+    if (timemillisec === 100) {
+      timemillisec = 0;
+      timesecond++;
+    }
+    timemillisec++;
+    return setTime({ sec: timesecond, milli: timemillisec });
+  };
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <div>Time:{time.sec + ":" + time.milli.toString().padStart(2, "0")}</div>
+      <button onClick={isRunning ? stopTime : startTime}>
+        {isRunning ? "Stop" : "Start"}
+      </button>
+      <button onClick={endTime}>End</button>
     </div>
-    <button onClick={startTime}>
-    {isRunning ? "Stop" : "Start"}
-    </button>
-    
-    <button>
-        End
-    </button>
-     </div>   
-    )
-}
-export default  Stopwatch
+  );
+};
+export default Stopwatch;
